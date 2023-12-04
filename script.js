59baa1601b031eaf49dcd17d57b00fe394ef4bd0
fire <102771014+Firespread01@users.noreply.github.com>
2023-12-04 10:21:11 -0800

    actual fix

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-    const editor = document.querySelector('.editor');
-    editor.value = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        </head>
-        <body>
-        
-        </body>
-        </html>
-        `;
+const editor = document.querySelector('.editor');
+editor.value = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    </head>
+    <body>
+
+    </body>
+    </html>
+    `;
 function run() {
     document.querySelector('.output').innerHTML = editor.value;
 }
